5fcf08726da9648750faa1fe0626af244e4ba373
vlad-karnauch <vlad-karnauch>
2022-09-09 16:27:10 +0300

    styled components

---
+++ src/_wcs/boards/styled-components.board.tsx
@@ -0,0 +1,24 @@
+import { createBoard } from '@wixc3/react-board';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background-color: #FAFBFC;
+    border: 2px solid rgba(27, 31, 35, 0.15);
+    border-radius: 8px;
+    color: #24292E;
+    font-size: 1rem;
+    padding: 18px;
+    transition border-color 0.7s;
+    &:hover {
+        border-color: #405cf5;
+    } 
+`;
+
+export default createBoard({
+    name: 'Styled Components',
+    Board: () => (
+        <div>
+            <Button>Styled-Components Button</Button>
+        </div>
+    )
+});
