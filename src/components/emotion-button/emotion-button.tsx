007386ea304e3f93ccca33f6c15d60147f52c9d0
vlad-karnauch <vlad-karnauch>
2022-09-09 18:28:30 +0300

    cursor

--- src/components/emotion-button/emotion-button.tsx
+++ src/components/emotion-button/emotion-button.tsx
@@ -8,10 +8,20 @@ export interface EmotionButtonProps {
 export const EmotionButton: React.FC<EmotionButtonProps> = ({ children }) => (
     <button
         css={{
-            backgroundColor: 'hotpink',
-            // '&:hover': {
-            //     color: 'lightgreen',
-            // },
+            backgroundColor: '#FAFBFC',
+            border: '2px solid rgba(27, 31, 35, 0.15)',
+            borderRadius: 8,
+            color: '#24292E',
+            fontSize: '1rem',
+            padding: 18,
+            transitionProperty: 'border-color, color',
+            transitionDuration: '0.7s',
+            cursor: 'pointer',
+            '&:hover': {
+                borderColor: '#405cf5',
+                color: '#405cf5'
+            }
+
         }}
     >
         {children}
