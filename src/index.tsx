f37f70e34e4770232254c0b10b5051b74364f0b0
vlad-karnauch <vlad-karnauch>
2022-09-11 09:30:08 +0300

    rename to docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './globals.st.css';
-import { App } from './app';
 
 createRoot(document.body.appendChild(document.createElement('div'))).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <React.StrictMode>Loading</React.StrictMode>
 );
